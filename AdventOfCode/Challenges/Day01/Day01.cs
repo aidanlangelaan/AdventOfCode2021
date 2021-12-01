7e1f6fd59b2723333cbdb824cd31f8899e3a10bf
Aidan Langelaan <aidanlangelaan@gmail.com>
2021-12-01 11:57:58 +0100

    Project setup

---
+++ AdventOfCode/Challenges/Day01/Day01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Challenges.Day01
+{
+    public class Day01
+    {
+        /// TODO
+        /// import day 1 input data
+        /// loop through all numbers and increase counter for each increase
+    }
+}
