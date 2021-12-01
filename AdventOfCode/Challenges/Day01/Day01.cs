2ffcbe14bbc558d9130f6ec4a7638a1a9cc11815
Aidan Langelaan <aidanlangelaan@gmail.com>
2021-12-01 22:53:58 +0100

    even shorter

--- AdventOfCode/Challenges/Day01/Day01.cs
+++ AdventOfCode/Challenges/Day01/Day01.cs
@@ -4,17 +4,15 @@
     {
         private readonly int[] input;
 
-        public Day01() => input = File.ReadAllText("Challenges\\Day01\\Day01Input.txt").Split("\n").Select(i => int.Parse(i)).ToArray();
+        public Day01() => input = File.ReadAllLines("Challenges\\Day01\\Day01Input.txt").Select(i => int.Parse(i)).ToArray();
 
         public int SolvePart1()
         {
             var count = 0;
-            for (var i = 0; i < input.Length; i++)
+            for (var i = 1; i < input.Length; i++)
             {
-                if (input[i] > input[i+1])
-                {
+                if (input[i] > input[i-1])
                     count++;
-                }
             }
             return count;
         }
@@ -22,23 +20,11 @@
         public int SolvePart2()
         {
             var count = 0;
-            var previous = input.Take(3).Sum();
-            for (var i = 0; i < input.Length; i++)
+            for (var i = 0; i < input.Length-3; i++)
             {
-                var window = input.Skip(i).Take(3);
-                if (window.Count() == 3)
-                {
-                    var current = window.Sum();
-                    if (current > previous)
-                    {
-                        count++;
-                    }
-                    previous = current;
-                }
-                else
-                {
-                    break;
-                }
+                var nextWindow = input.Skip(i+1).Take(3);
+                if (nextWindow.Sum() > input.Skip(i).Take(3).Sum())
+                    count++;
             }
             return count;
         }
