aa23f7b076c3930d2c6f5d3609853247b604fe65
Aidan Langelaan <aidanlangelaan@gmail.com>
2021-12-03 09:06:14 +0100

    Completed day 3 - part 1

--- AdventOfCode/Challenges/Day03/Day03.cs
+++ AdventOfCode/Challenges/Day03/Day03.cs
@@ -8,12 +8,39 @@
 
         public int SolvePart1()
         {
-            return 0;
+            var mostCommonNumberStr = string.Empty;
+            for(var i = 1; i < input[0].Length; i++)
+            {
+                var mostCommonNumber = GetCommonNumber(i - 1);
+                mostCommonNumberStr = $"{mostCommonNumberStr}{mostCommonNumber}";
+            }
+
+            var leastCommonNumberStr = string.Empty;
+            foreach (var number in mostCommonNumberStr)
+            {
+                var convert = number == '0' ? '1' : '0';
+                leastCommonNumberStr = $"{leastCommonNumberStr}{convert}";
+            }
+
+            return (Convert.ToInt32(mostCommonNumberStr, 2) * 2) * (Convert.ToInt32(leastCommonNumberStr, 2) * 2);
         }
 
         public int SolvePart2()
         {
             return 0;
+        }
+
+        public int GetCommonNumber(int column)
+        {
+            var countZero = 0;
+            var countOne = 0;
+            for (var i = 0; i < input.Length; i++)
+            {
+                var value = input[i].Substring(column, 1);
+                if (value == "0") countZero++;
+                if (value == "1") countOne++;
+            }
+            return countOne > countZero ? 1 : 0;
         }
     }
 }
