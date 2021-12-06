20a418f437a8f819920a1b0b85e9a4a9e428c1ba
Aidan Langelaan <aidanlangelaan@gmail.com>
2021-12-06 07:40:21 +0100

    Day 06 setup

---
+++ AdventOfCode/Challenges/Day06/Day06.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.Challenges
+{
+    public class Day06
+    {
+        private readonly string[] input;
+
+        public Day06(string[] Input) => input = Input;
+
+        public int SolvePart1()
+        {
+            return 0;
+        }
+
+        public int SolvePart2()
+        {
+            return 0;
+        }
+    }
+}
